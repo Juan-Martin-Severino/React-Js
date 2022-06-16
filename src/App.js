805ed26f2c1724cda4ed9a6bbed382ca79b006d1
Juan Martin Severino <juanmartinseverino@outlook.com>
2022-06-15 23:40:55 -0300

    crear la app utilizando el cli

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a Estudio Color!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/estudiocolor/?hl=es"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nuestro Instagram!
         </a>
       </header>
     </div>
