2926b59780db885bd55aaeb6869c23e95bb48ca1
Juan Martin Severino <juanmartinseverino@outlook.com>
2022-06-28 03:19:01 -0300

    contador con boton

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({ greeting }) => {
   return (
     <>
       <h4>{greeting}</h4>
+      <ItemCount stock={10} initial={1} />
     </>
   );
 };
